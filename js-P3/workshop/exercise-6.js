cd741525539723ad7b43da5e4b7ca6acbb716c30
youngGaf <47207704+youngGaf@users.noreply.github.com>
2023-07-16 02:37:44 -0700

    Add files via upload

---
+++ js-P3/workshop/exercise-6.js
@@ -0,0 +1,11 @@
+// Exercise 6
+// ---------------
+// Write an arrow function accepts an array of values and returns a new array of stringified values.
+//
+
+const convertToString = (arr) => {
+  // Insert missing solution please
+};
+
+// We need to export the function in order for our unit test to have access to it.
+module.exports = convertToString;
